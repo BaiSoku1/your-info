6bc93db82130e8eaf71f0b69c01b66ceac8e6649
BaiSoku1 <baisoku24@gmail.com>
2026-04-22 23:41:56 +0800

    Update mangled_shuffled.lua
    Signed-off-by: BaiSoku1 <baisoku24@gmail.com>

--- Prometheus/src/prometheus/namegenerators/mangled_shuffled.lua
+++ Prometheus/src/prometheus/namegenerators/mangled_shuffled.lua
@@ -4,32 +4,31 @@
 --
 -- This Script provides a function for generation of mangled names with shuffled character order
 
+local util = require("prometheus.util")
 
-local util = require("prometheus.util");
-local chararray = util.chararray;
+local VarDigits = util.chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
+local VarStartDigits = util.chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_");
-local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-local function generateName(id, _)
-	local name = ''
-	local d = id % #VarStartDigits
-	id = (id - d) / #VarStartDigits
-	name = name..VarStartDigits[d+1]
-	while id > 0 do
-		local e = id % #VarDigits
-		id = (id - e) / #VarDigits
-		name = name..VarDigits[e+1]
-	end
-	return name
+local function generateName(id)
+    local chars = {}
+    local d = id % #VarStartDigits + 1
+    id = math.floor(id / #VarStartDigits)
+    chars[1] = VarStartDigits[d]
+    
+    local idx = 2
+    while id > 0 do
+        local e = id % #VarDigits + 1
+        id = math.floor(id / #VarDigits)
+        chars[idx] = VarDigits[e]
+        idx = idx + 1
+    end
+    
+    return table.concat(chars)
 end
 
-local function prepare(_)
-	util.shuffle(VarDigits);
-	util.shuffle(VarStartDigits);
+local function prepare()
+    util.shuffle(VarDigits)
+    util.shuffle(VarStartDigits)
 end
 
-return {
-	generateName = generateName,
-	prepare = prepare
-};
+return { generateName = generateName, prepare = prepare }
